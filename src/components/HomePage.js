67c264a1d099d5ca52d1a8a0461aa658f0dc5231
juliogamez94 <juliogamezb@gmail.com>
2022-09-06 13:47:50 -0600

    Added dropdown filter, populate item-card and link by clicking the item-card

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,42 +1,71 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import Select from "react-select";
 import NewsItem from "./NewsItem";
 
 const HomePage = () => {
   const [articles, setArticles] = useState([]);
+  const [query, setQuery] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [totalPages, setTotalPages] = useState(0);
+
+  const optionsNews = [
+    {
+      label: "React",
+      value: "react",
+    },
+    {
+      label: "Angular",
+      value: "angular",
+    },
+    {
+      label: "Vue",
+      value: "vue",
+    },
+  ];
+
+  const handleSelectChange = ({ value }) => {
+    setQuery(value);
+  };
 
   useEffect(() => {
     setIsLoading(true);
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(
-          "https://hn.algolia.com/api/v1/search?"
-        );
-        const { hits, nbPages } = data;
 
-        setArticles(hits);
-        setTotalPages(nbPages);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://hn.algolia.com/api/v1/search_by_date?query=${query}&page=0`
+      );
+      const data = await res.json();
+      setArticles(data.hits);
+      setIsLoading(false);
     };
+
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
-      <h1>Hacker News</h1>
+      <h1 className="header">Hacker News</h1>
+      <div className="options">
+        <Select
+          defaultValue={{ label: "Select your news", value: " " }}
+          options={optionsNews}
+          onChange={handleSelectChange}
+        />
+      </div>
       <div className="item-container">
         {isLoading ? (
           <p>Loading....</p>
         ) : (
-          articles.map((article) => (
-            <NewsItem key={article.objectID} article={article} />
-          ))
+          articles.map(
+            ({ author, created_at, story_title, story_url, objectID }) => (
+              <NewsItem
+                key={objectID}
+                title={story_title}
+                url={story_url}
+                author={author}
+                createdAt={created_at}
+              />
+            )
+          )
         )}
       </div>
     </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,31 @@
+import { intlFormatDistance } from "date-fns";
 import React from "react";
+import { BsClock } from "react-icons/bs";
 
-const NewsItem = ({ article }) => {
-  if (!article.title) return null;
+const NewsItem = ({ author, createdAt, title, url }) => {
+  if (!title) return null;
+
+  const openInNewTab = () => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
   return (
-    <div className="item-card">
-      <h3>{article.title}</h3>
-    </div>
+    <>
+      <article>
+        <div className="item-card" onClick={openInNewTab}>
+          <span className="created-by">
+            <span className="clock">
+              <BsClock />
+            </span>
+            {intlFormatDistance(new Date(createdAt), new Date(), {
+              locale: "en",
+            }) + " "}
+            by{" " + author}
+          </span>
+          <h3>{title}</h3>
+        </div>
+      </article>
+    </>
   );
 };
 
